14f57102883008c5d76e45f5c0ee671e579a8c4a
Chedak <cedomir.miskov@gmail.com>
2019-02-01 17:53:47 +0100

    Remove text output commands

--- Cheda/bubble_sort.rb
+++ Cheda/bubble_sort.rb
@@ -1,12 +1,8 @@
 def bubble_sort(a)
-print a
-puts ""
   i = 0
   j = 0
   while i < a.length do
-
     while j < (a.length - i -1) do
-
         if a[j] > a[j+1]
           temp = a[j+1]
           a[j+1] = a[j]
@@ -16,7 +12,7 @@ puts ""
     end
     i += 1
   end
-  print a
+return a
 end
 
 test_array = [5,1,3,6,7,8,9,5]
